abe9ab145adefcfb215bb12756f0367e6f0c48cb
Artem Lipatov <houndvibe@gmail.com>
2025-06-19 18:14:15 +0700

    table refactoring

--- client/src/components/ui/SubCategoryContent/Table.tsx
+++ client/src/components/ui/SubCategoryContent/Table.tsx
@@ -5,58 +5,51 @@ import type {
   TalismansSubCategoryMap,
 } from "../../../global-types";
 import { useAppDispatch, useAppSelector } from "../../../store/typedDispatch";
-import { CheckOutlined } from "@ant-design/icons";
-
-import styles from "./SubCategoryContent.module.scss";
+import { CheckOutlined, ThunderboltTwoTone } from "@ant-design/icons";
 import {
   Table as AntdTable,
   Checkbox,
   Flex,
   Image,
   Tooltip,
-  type TablePaginationConfig,
   type TableProps,
 } from "antd";
+import Link from "antd/es/typography/Link";
+import styles from "./SubCategoryContent.module.scss";
+import dlcIcon from "../../../assets/dlc-icon.png";
+
 import { getNextSortStep, smartNameSort } from "../../../lib/utils/sorters";
 import { checkIsLegendary, hasVersionsProperty } from "../../../lib/utils/misc";
 import { APP_PALETTE } from "../../../lib/consts";
-import Link from "antd/es/typography/Link";
-import type {
-  FilterValue,
-  SorterResult,
-  SortOrder,
-} from "antd/es/table/interface";
-import { ThunderboltTwoTone } from "@ant-design/icons";
-import dlcIcon from "../../../assets/dlc-icon.png";
 import { toggleTalismanCollected } from "../../../store/collectionSlice";
 import { getStoreAction } from "../../../store/actions";
 import { setGlobalSearchItem } from "../../../store/serviceSlice";
+import type { SortOrder } from "antd/es/table/interface";
+
+interface Props {
+  setHoveredItemName: React.Dispatch<React.SetStateAction<string>>;
+  dataSource: Item[];
+  subcategory: ItemSubCategory;
+}
 
 export default function Table({
   setHoveredItemName,
   dataSource,
   subcategory,
-}: {
-  setHoveredItemName: React.Dispatch<React.SetStateAction<string>>;
-  dataSource: Item[];
-  subcategory: ItemSubCategory;
-}) {
+}: Props) {
   const dispatch = useAppDispatch();
-
   const { globalSearchItem } = useAppSelector((state) => state.service);
 
-  const [sortStep, setSortStep] = useState<number>(0);
+  const [sortStep, setSortStep] = useState(0);
   const [sortColumn, setSortColumn] = useState<string | null>(null);
-  const [sortOrder, setSortOrder] = useState<"ascend" | "descend" | null>(null);
+  const [sortOrder, setSortOrder] = useState<SortOrder>(null);
 
-  //debounce
   const hoverTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
 
   const handleMouseEnter = (record: Item) => {
     if (record.name === globalSearchItem) {
       dispatch(setGlobalSearchItem(null));
     }
-
     hoverTimeoutRef.current = setTimeout(() => {
       setHoveredItemName(record.name);
     }, 50);
@@ -69,6 +62,103 @@ export default function Table({
     }
   };
 
+  const renderNameCell = (value: string, record: Item) => {
+    const getLink = (type: "link" | "legendary") => {
+      return (
+        <Link
+          href={record.link}
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className={styles[type]}>{value}</div>
+        </Link>
+      );
+    };
+
+    return (
+      <Flex gap={10}>
+        {checkIsLegendary(record) ? (
+          <>
+            <Tooltip title="Legendary Item">
+              <ThunderboltTwoTone twoToneColor={APP_PALETTE.textPrimary} />
+            </Tooltip>
+            {getLink("legendary")}
+          </>
+        ) : (
+          getLink("link")
+        )}
+        {record.dlc && (
+          <Tooltip title="Shadow of the Erdtree Dlc content">
+            <Image src={dlcIcon} height={20} />
+          </Tooltip>
+        )}
+      </Flex>
+    );
+  };
+
+  const renderCollectedCell = (_: boolean, record: Item) => {
+    if (hasVersionsProperty(record)) {
+      return (
+        <Flex vertical gap={1}>
+          {record.versions.map((item) => (
+            <Flex key={item.tier} align="center" gap={4}>
+              <Checkbox
+                checked={item.collected}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  dispatch(
+                    toggleTalismanCollected({
+                      subcategory: subcategory as keyof TalismansSubCategoryMap,
+                      name: record.name,
+                      tier: item.tier,
+                    })
+                  );
+                }}
+              />
+              {item.legendary ? (
+                <>
+                  <div className={styles.legendary}>
+                    {item.tier > 0 && `+${item.tier}`}
+                  </div>
+                  <Tooltip title="Legendary Item">
+                    <ThunderboltTwoTone
+                      twoToneColor={APP_PALETTE.textPrimary}
+                    />
+                  </Tooltip>
+                </>
+              ) : (
+                item.tier > 0 && `+${item.tier}`
+              )}
+              {item.dlc && (
+                <Tooltip title="Shadow of the Erdtree Dlc content">
+                  <Image src={dlcIcon} height={12} preview={false} />
+                </Tooltip>
+              )}
+            </Flex>
+          ))}
+        </Flex>
+      );
+    }
+
+    return (
+      <Flex gap={5} align="baseline">
+        <Checkbox
+          checked={record.collected}
+          onClick={(e) => {
+            e.stopPropagation();
+            getStoreAction({
+              name: record.name,
+              category: record.type,
+              subcategory,
+              dispatch,
+            });
+          }}
+        />
+      </Flex>
+    );
+  };
+
   const columns: TableProps<Item>["columns"] = [
     {
       title: "Name",
@@ -77,133 +167,43 @@ export default function Table({
       width: "85%",
       sortOrder: sortColumn === "name" ? "ascend" : null,
       sorter: (a, b) => smartNameSort(sortStep, a, b),
-      render: (value, record) => {
-        return (
-          <Flex gap={10}>
-            {checkIsLegendary(record) ? (
-              <>
-                <Tooltip title={"Legendary Item"}>
-                  <ThunderboltTwoTone twoToneColor={APP_PALETTE.textPrimary} />
-                </Tooltip>
-                <Link
-                  href={record.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={(e) => e.stopPropagation()}
-                >
-                  <div className={styles.legendary}>{value}</div>
-                </Link>
-              </>
-            ) : (
-              <Link
-                href={record.link}
-                target="_blank"
-                rel="noopener noreferrer"
-                onClick={(e) => e.stopPropagation()}
-              >
-                <span className={styles.link}>{value}</span>
-              </Link>
-            )}
-            {record.dlc && (
-              <Tooltip title={"Shadow of the Erdtree Dlc content"}>
-                <Image src={dlcIcon} height={20} />
-              </Tooltip>
-            )}
-          </Flex>
-        );
-      },
+      render: renderNameCell,
     },
     {
       title: <CheckOutlined />,
       dataIndex: "collected",
       key: "collected",
-
       sortOrder: sortColumn === "collected" ? sortOrder : null,
       sorter: (a, b) =>
         a.collected === b.collected ? 0 : a.collected ? -1 : 1,
-      render: (_value: boolean, record: Item) => {
-        return hasVersionsProperty(record) ? (
-          <Flex vertical gap={1}>
-            {record.versions.map((item) => (
-              <Flex key={item.tier} align="center" gap={4}>
-                <Checkbox
-                  checked={item.collected}
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    dispatch(
-                      toggleTalismanCollected({
-                        subcategory:
-                          subcategory as keyof TalismansSubCategoryMap,
-                        name: record.name,
-                        tier: item.tier,
-                      })
-                    );
-                  }}
-                />
-                {item.legendary ? (
-                  <>
-                    <div className={styles.legendary}>
-                      {item.tier > 0 && `+${item.tier}`}
-                    </div>
-                    <Tooltip title={"Legendary Item"}>
-                      <ThunderboltTwoTone
-                        twoToneColor={APP_PALETTE.textPrimary}
-                      />
-                    </Tooltip>
-                  </>
-                ) : (
-                  item.tier > 0 && `+${item.tier}`
-                )}
-
-                {item.dlc && (
-                  <Tooltip title={"Shadow of the Erdtree Dlc content"}>
-                    <Image src={dlcIcon} height={12} preview={false} />
-                  </Tooltip>
-                )}
-              </Flex>
-            ))}
-          </Flex>
-        ) : (
-          <Flex gap={5} align="baseline">
-            <Checkbox
-              checked={record.collected}
-              onClick={(e) => {
-                e.stopPropagation();
-                getStoreAction({
-                  name: record.name,
-                  category: record.type,
-                  subcategory: subcategory,
-                  dispatch: dispatch,
-                });
-              }}
-            />
-          </Flex>
-        );
-      },
+      render: renderCollectedCell,
     },
   ];
 
-  function onChangeTable(
-    _pagination: TablePaginationConfig,
-    _filters: Record<string, FilterValue | null>,
-    sorter: SorterResult<Item> | SorterResult<Item>[]
-  ) {
-    const currentSorter = Array.isArray(sorter) ? sorter[0] : sorter;
+  const handleTableChange: TableProps<Item>["onChange"] = (
+    _pagination,
+    _filters,
+    sorter
+  ) => {
+    const sortObj = Array.isArray(sorter) ? sorter[0] : sorter;
 
-    if (currentSorter.columnKey === "name") {
-      setSortStep((prev) => getNextSortStep(dataSource, prev));
-      setSortColumn("name");
-      setSortOrder("ascend");
-    } else if (currentSorter.columnKey === "collected") {
-      setSortStep(0);
-      setSortColumn("collected");
-      setSortOrder((currentSorter.order ?? null) as SortOrder);
-    } else {
-      setSortStep(0);
-      setSortColumn(null);
-      setSortOrder(null);
+    switch (sortObj.columnKey) {
+      case "name":
+        setSortStep((prev) => getNextSortStep(dataSource, prev));
+        setSortColumn("name");
+        setSortOrder("ascend");
+        break;
+      case "collected":
+        setSortStep(0);
+        setSortColumn("collected");
+        setSortOrder(sortObj.order ?? null);
+        break;
+      default:
+        setSortStep(0);
+        setSortColumn(null);
+        setSortOrder(null);
     }
-  }
+  };
 
   return (
     <AntdTable
@@ -214,31 +214,33 @@ export default function Table({
       size="small"
       rowKey="name"
       rowClassName={(record) => {
+        if (record.name === globalSearchItem) return "row-searchTarget";
         if (record.collected) return "row-collected";
-        if (globalSearchItem === record.name) return "row-searchTarget";
+
         return "row-missing";
       }}
-      onChange={onChangeTable}
+      onChange={handleTableChange}
       onRow={(record) => ({
         onMouseEnter: () => handleMouseEnter(record),
-        onMouseLeave: () => handleMouseLeave(),
+        onMouseLeave: handleMouseLeave,
         onClick: () => {
           if (record.type === "talismans") {
-            // Только если нет versions, переключаем весь talisman
-            if (!("versions" in record) || record.versions?.length === 0) {
+            if (!("versions" in record) || !record.versions?.length) {
               dispatch(
                 toggleTalismanCollected({
                   subcategory: subcategory as keyof TalismansSubCategoryMap,
                   name: record.name,
                 })
               );
+            } else {
+              return;
             }
           } else {
             getStoreAction({
               name: record.name,
               category: record.type,
-              subcategory: subcategory,
-              dispatch: dispatch,
+              subcategory,
+              dispatch,
             });
           }
         },
